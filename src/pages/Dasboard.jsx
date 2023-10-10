947fbcbe117cbf531206bbcd80b36b0e72f5edfa
Shehan <34shehan@gmail.com>
2023-10-10 14:29:08 +0530

    changed

--- src/pages/Dasboard.jsx
+++ src/pages/Dasboard.jsx
@@ -2,8 +2,56 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="p-4">
-      <h1>Dashboard Content</h1>
+    <div
+      className="container-fluid min-vh-100 mt-4"
+      style={{ background: "#E0E0E0" }}
+    >
+      <div className="row">
+        <div className="col-md-4 mb-4">
+          <div className="card bg-primary text-white shadow-sm rounded p-3">
+            <div className="d-flex justify-content-between align-items-center">
+              <div>
+                <h3 className="fs-2 mb-0">230</h3>
+                <p className="fs-5">Products</p>
+              </div>
+              <i className="bi bi-cart-plus fs-1"></i>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card bg-success text-white shadow-sm rounded p-3">
+            <div className="d-flex justify-content-between align-items-center">
+              <div>
+                <h3 className="fs-2 mb-0">230</h3>
+                <p className="fs-5">Products</p>
+              </div>
+              <i className="bi bi-cart-plus fs-1"></i>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card bg-info text-white shadow-sm rounded p-3">
+            <div className="d-flex justify-content-between align-items-center">
+              <div>
+                <h3 className="fs-2 mb-0">230</h3>
+                <p className="fs-5">Products</p>
+              </div>
+              <i className="bi bi-cart-plus fs-1"></i>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 mb-4">
+          <div className="card bg-info text-white shadow-sm rounded p-3">
+            <div className="d-flex justify-content-between align-items-center">
+              <div>
+                <h3 className="fs-2 mb-0">230</h3>
+                <p className="fs-5">Products</p>
+              </div>
+              <i className="bi bi-cart-plus fs-1"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
